e638f67938aecc944cd8c03dd3c92fda3a84c901
zafaremon20 <zafar emon>
2022-06-17 02:00:58 +0600

    create get method and post method and post data from client side

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 port = process.env.PORT || 5000;
-
-// nikeWarehouse
-// yTpU4k5cXhzK7bqR
 
 // middleware 
 app.use(cors());
 app.use(express.json());
+
 
 app.get('/', (req, res) => {
   res.send('Hello Word!')
@@ -19,13 +17,38 @@ app.get('/', (req, res) => {
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@nikewarehouse.eonwx.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log('nike warehouse connected');
-  // perform actions on the collection object
-  client.close();
-});
 
+async function run() {
+  try {
+    await client.connect();
+    const productCollection = client.db('NikeWarehouse').collection('Product');
+
+    app.get('/product', async (req, res) => {
+      const query = {};
+      const cursor = productCollection.find(query);
+      const product = await cursor.toArray();
+      res.send(product);
+    });
+
+    app.get('/product/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const product = await productCollection.findOne(query);
+      res.send(product);
+    })
+
+    // POST
+    app.post('/product', async (req, res) => {
+      const newProduct = req.body;
+      const result = await productCollection.insertOne(newProduct);
+      res.send(result);
+    })
+  }
+  finally {
+
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log('Listening to port', port);

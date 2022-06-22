f8d44c298eabf241ca99eb8b6ffe28fa4a6909b8
zafaremon20 <zafar emon>
2022-06-22 11:09:29 +0600

    set jwt but its uncomplete

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonWebToken');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
@@ -23,6 +24,14 @@ async function run() {
     await client.connect();
     const productCollection = client.db('NikeWarehouse').collection('Product');
 
+    // AUTH 
+    app.post('/login', async (req, res) => {
+      const user = req.body;
+      const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '1d'
+      })
+      res.send({ accessToken });
+    })
 
     // READ OR GET
     app.get('/product', async (req, res) => {

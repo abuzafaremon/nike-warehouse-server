2ab69ae8cc352fab72f28a9e02b0b56551903597
zafaremon20 <zafar emon>
2023-02-12 12:28:14 +0600

    done

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
-port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
 // middleware 
 app.use(cors());
